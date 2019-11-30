04b8f52f750d02f558a318804a3663748a0ca9f7
aleks <delov.alesha@yandex.ru>
2019-11-30 15:29:21 +0300

    fix soap

--- MachineLearningService/urls.py
+++ MachineLearningService/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('regr/', views.regr, name="regr"),
+    url(r'^soap_service/', views.my_soap_application),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import numpy as np
 
+from django.views.decorators.csrf import csrf_exempt
+from spyne.application import Application
+from spyne.decorator import rpc
+from spyne.model.primitive import Unicode, Integer
+from spyne.protocol.soap import Soap11
+from spyne.server.django import DjangoApplication
+from spyne.service import ServiceBase
+
 # Create your views here.
 from sklearn.linear_model import LinearRegression
 
@@ -43,3 +51,24 @@ def regr(request):
     y_pred = clf.predict(X_test)
 
     return HttpResponse(y_pred)
+
+
+class SoapService(ServiceBase):
+    @rpc(Unicode(nillable=False), _returns=Unicode)
+    def hello(ctx, name):
+        return 'Hello, {}'.format(name)
+
+    @rpc(Integer(nillable=False), Integer(nillable=False), _returns=Integer)
+    def sum(ctx, a, b):
+        return int(a + b)
+
+
+soap_app = Application(
+    [SoapService],
+    tns='django.soap.example',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11(),
+)
+
+django_soap_application = DjangoApplication(soap_app)
+my_soap_application = csrf_exempt(django_soap_application)
